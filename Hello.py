0a5b9f9f846a7878c61a8168c90aecc2902b903f
youngho <9479750@gmail.com>
2018-10-27 03:04:13 +0900

    Hello

--- Hello.py
+++ Hello.py
@@ -1,2 +1,18 @@
+import requests as req
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+
 print('Hello!')
 print(('My name is youngho'))
+url = "https://www.naver.com"
+html = req.get(url).text
+#print(html)
+soup = BeautifulSoup(html, "html.parser")
+a = soup.find_all('img')
+
+filePath = 'e:/imagedown/'
+
+for i,e in enumerate(a,1):
+    #print(i['src'])
+    urllib.request.urlretrieve(e['src'], os.path.join(filePath + str(i) + '.jpg'))
